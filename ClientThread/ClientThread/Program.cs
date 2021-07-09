f6df648071461c1e580f909005def3c1c317f318
hikmatul ulya <72594758+hikmah-ulya@users.noreply.github.com>
2021-07-09 23:47:04 +0700

    Add files via upload

---
+++ ClientThread/ClientThread/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ClientThread
+{
+  class Read
+   {
+    public void read(object obj)
+     {
+      TcpClient tcpClient = (TcpClient)obj;
+      StreamReader sReader = new StreamReader(tcpClient.GetStream());
+      while(true)
+      {
+        try
+        {
+         // baca pesan masuk
+         string message = sReader.ReadLine();
+         Console.WriteLine(message);
+         
+         }
+         catch (Exception e)
+         {
+         Console.WriteLine(e.Message);
+         break;
+         }
+        }
+      }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Read reader = new Read();
+            try
+            {
+                //this computer  
+                TcpClient tcpClient = new TcpClient("127.0.0.1", 5000);
+                Console.WriteLine("Connected to server, now input your name!");
+
+                Thread thread = new Thread(reader.read);
+                thread.Start(tcpClient);
+
+                StreamWriter sWriter = new StreamWriter(tcpClient.GetStream());
+
+                while (true)
+                {
+                    if (tcpClient.Connected)
+                    {
+                        //kirim pesan
+                        string input = Console.ReadLine();
+                        sWriter.WriteLine(input);
+                        sWriter.Flush();
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ServerThread/ServerThread/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ServerThread
+{
+    class HandleClient
+    {
+        Program program = new Program();
+        public void Announce(object obj)
+        {
+            TcpClient tcpClient = (TcpClient)obj;
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+
+            while (true)
+            {
+                //server broadcast 
+                string message = Console.ReadLine();
+                BroadCast(message, tcpClient, null);
+                string chat = "Announce : " + message;
+                Console.WriteLine(chat);
+
+                //save to txt
+                program.writeMessage(chat);
+            }
+        }
+
+        public void ClientListener(object obj)
+        {
+                TcpClient tcpClient = (TcpClient)obj;
+                StreamReader reader = new StreamReader(tcpClient.GetStream());
+                string name = reader.ReadLine();
+
+                Console.WriteLine(name + " connected");
+                while (true)
+                {
+                    try
+                    {
+                        //record chat  
+                        string message = reader.ReadLine();
+                        BroadCast(message, tcpClient, name);
+                        string chat = "Client " + name + " : " + message;
+                        Console.WriteLine(chat);
+
+                        //save to txt
+                        program.writeMessage(chat);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        program.removeClient(tcpClient);
+                        break;
+                    }
+                }
+        }
+        
+
+
+        public void BroadCast(string msg, TcpClient excludeClient, string n)
+        {
+            foreach (TcpClient client in Program.tcpClientsList)
+            {
+                //broadcast to all client except sender
+                StreamWriter sWriter = new StreamWriter(client.GetStream());
+                if (n == null)
+                {
+                    sWriter.WriteLine("Server : " + msg);
+                }
+                else if (client != excludeClient)
+                {
+                    sWriter.WriteLine("Client " + n + " : " + msg);
+                }
+                sWriter.Flush();
+            }
+        }
+    }
+    class Program
+    {
+        public static TcpListener tcpListener;
+        public static List<TcpClient> tcpClientsList = new List<TcpClient>();
+        private List<string> messagesToSave = new List<string>();
+
+        static void Main(string[] args)
+        {
+            HandleClient handleClient = new HandleClient();
+
+            //start process
+            tcpListener = new TcpListener(IPAddress.Any, 5000);
+            tcpListener.Start();
+            Console.WriteLine("Server created");
+            while (true)
+            {
+                //add clients to list
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                tcpClientsList.Add(tcpClient);
+
+                //broadcast from server
+                Thread bc = new Thread(() => handleClient.Announce(tcpClient));
+                bc.Start();
+
+                //start listener
+                Thread startListen = new Thread(() => handleClient.ClientListener(tcpClient));
+                startListen.Start();
+            }
+        }
+
+        public void writeMessage(string chat)
+        {
+            messagesToSave.Add(chat);
+            File.WriteAllLines("C:/Users/AnnaUlive/Documents/hikmah/ProjectAJK/chats.txt", messagesToSave);
+        }
+
+        public void removeClient(TcpClient client)
+        {
+            tcpClientsList.Remove(client);
+        }
+    }
+}
